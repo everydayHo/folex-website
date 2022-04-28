a99610abe21beb85aef5cfbe6cfc9b54b2506be8
everydayHo <jungwoopage@gmail.com>
2022-04-28 18:25:45 +0900

    토글메뉴,네브바스크롤색상변경

--- js/main.js
+++ js/main.js
@@ -1,4 +1,5 @@
 'use strict';
+
 // fullpage
 $(document).ready(function () {
 	$('#fullpage').fullpage({
@@ -10,8 +11,14 @@ $(document).ready(function () {
 
 			if (destination > 1) {
 				$('.fixed-nav').fadeIn();
+				$('.header nav').addClass('active');
+				$('.logo img').attr('src', './images/logo-dark.png');
+				$('.toggle-btn img').attr('src', './images/menu_icon-dark.png');
 			} else {
 				$('.fixed-nav').fadeOut();
+				$('.header nav').removeClass('active');
+				$('.logo img').attr('src', './images/logo.png');
+				$('.toggle-btn img').attr('src', './images/menu_icon.png');
 			}
 			if (destination === 6) {
 				$('.back_to_top').addClass('active');
@@ -26,7 +33,6 @@ $(document).ready(function () {
 				.addClass('active');
 		},
 	});
-
 	//methods
 	$.fn.fullpage.setAllowScrolling(true);
 });
@@ -47,4 +53,12 @@ const swiper = new Swiper('.swiper', {
 		nextEl: '.swiper-button-next',
 		prevEl: '.swiper-button-prev',
 	},
+});
+
+// toggle menu
+$('.toggle-btn').click(function () {
+	$('.toggle_menu').addClass('active');
+});
+$('.toggle_menu .close').click(function () {
+	$('.toggle_menu').removeClass('active');
 });
